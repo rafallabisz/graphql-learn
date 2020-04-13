d553f7b73f70b75394ce46cda7ab08f4112f3f74
Rafal Labisz <rafal0710@gmail.com>
2020-04-13 20:47:58 +0200

    extract args mutation register to RegisterInput

---
+++ src/modules/user/register/RegisterInput.ts
@@ -0,0 +1,17 @@
+import { MaxLength, Length } from 'class-validator';
+import { Field, InputType } from 'type-graphql';
+
+@InputType()
+export class RegisterInput {
+  @Field()
+  firstName: string;
+
+  @Field()
+  lastName: string;
+
+  @Field()
+  email: string;
+
+  @Field()
+  password: string;
+}
